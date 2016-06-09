01704de14a7a5cd5b4c17abc821cbddf3041f7ad
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-06-08 17:45:06 -0700

    update start up script to be more robust

--- main.js
+++ main.js
@@ -1,53 +1,101 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const path = require('path')
+const glob = require('glob')
+const electron = require('electron')
+const autoUpdater = require('./auto-updater')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+const debug = /--debug/.test(process.argv[2])
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+var mainWindow = null
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+function initialize () {
+  var shouldQuit = makeSingleInstance()
+  if (shouldQuit) return app.quit()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  function createWindow () {
+    var windowOptions = {
+      width: 1080,
+      minWidth: 680,
+      height: 840
+    }
+
+    if (process.platform === 'linux') {
+      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+
+    // Launch fullscreen with DevTools open, usage: npm run debug
+    if (debug) {
+      mainWindow.webContents.openDevTools()
+      mainWindow.maximize()
+    }
+
+    mainWindow.on('closed', function () {
+      mainWindow = null
+    })
+  }
+
+  app.on('ready', function () {
+    createWindow()
+    autoUpdater.initialize()
+  })
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', function () {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// Make this app a single instance app.
+//
+// The main window will be restored and focused instead of a second window
+// opened when a person attempts to launch a second instance.
+//
+// Returns true if the current version of the app should quit instead of
+// launching.
+function makeSingleInstance () {
+  if (process.mas) return false
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  return app.makeSingleInstance(function () {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+}
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+// Require each JS file in the main-process dir
+function loadDemos () {
+  var files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
+  files.forEach(function (file) {
+    require(file)
+  })
+  autoUpdater.updateMenu()
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Handle Squirrel on Windows startup events
+switch (process.argv[1]) {
+  case '--squirrel-install':
+    autoUpdater.createShortcut(function () { app.quit() })
+    break
+  case '--squirrel-uninstall':
+    autoUpdater.removeShortcut(function () { app.quit() })
+    break
+  case '--squirrel-obsolete':
+  case '--squirrel-updated':
+    app.quit()
+    break
+  default:
+    initialize()
+}
