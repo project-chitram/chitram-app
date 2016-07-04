949d683b67e75322bcba340736b1fe491a12eed3
Srinivasulu Punuru <spunuru@linkedin.com>
2016-07-04 15:24:11 -0700

    Getting the packaged app working

--- bash/create-video.sh
+++ bash/create-video.sh
@@ -4,4 +4,4 @@ scriptDir=$(dirname $0)
 screenshotter="${scriptDir}/../screenshotter.js"
 echo "creating video using ${screenshotter} for chart ${1} at ${2}..."
 
-phantomjs $screenshotter $1 3 | ffmpeg -y -c:v png -f image2pipe -r 25 -t 10  -i - -c:v libx264 -pix_fmt yuv420p -movflags +faststart $2
+/usr/local/bin/phantomjs $screenshotter $1 3 | /usr/local/bin/ffmpeg -y -c:v png -f image2pipe -r 25 -t 10  -i - -c:v libx264 -pix_fmt yuv420p -movflags +faststart $2
