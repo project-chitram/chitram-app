c501092276e1fd675e61c990345bfc23b0331641
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2016-07-03 21:21:25 -0700

    write data

--- renderer-process/windows/inputform.js
+++ renderer-process/windows/inputform.js
@@ -1,5 +1,6 @@
 const path = require('path');
 require('json-editor');
+var jsonfile = require('jsonfile')
 var fs = require('fs');
 var schemaFile = path.join(__dirname, '../../viz_templates/chitram_bar_chart/inputschema.json');
 var inputSchema = JSON.parse(fs.readFileSync(schemaFile, 'utf8'));
@@ -13,13 +14,20 @@ var editor = new JSONEditor(document.getElementById('editor-holder'),
 
 const previewBtn = document.getElementById('preview-button')
 previewBtn.addEventListener('click', function (event) {
-  console.log(event.target.dataset.template)
+  var file = require('path').join(__dirname, '../../viz_templates/'+event.target.dataset.template +'/data/data.json')
+  var obj = editor.getValue()
+  jsonfile.writeFile(file, obj)
   const BrowserWindow = require('electron').remote.BrowserWindow
-  const path = require('path')
-
-  const modalPath = path.join('http://localhost:90923/viz_templates/'+event.target.dataset.template +'/index.html')
-  let win = new BrowserWindow({ width: 1000, height: 600 })
+  const modalPath = 'http://127.0.0.1:60923/' + event.target.dataset.template +'/index.html'
+  console.log(modalPath)
+  let win = new BrowserWindow({ width: 1000,
+                                height: 600,
+                                nodeIntegration: false,
+                                webPreferences: {
+                                    webSecurity: false
+                                }
+                              })
   win.on('closed', function () { win = null })
-  win.loadURL(modalPath)
+  win.loadURL(modalPath, {"extraHeaders" : "pragma: no-cache\n"})
   win.show()
 });
